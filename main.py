2be2806ca6c7eb3bcd38e49e8af8bb6720382ac6
liav2002 <liavmail00@gmail.com>
2025-01-05 15:39:03 +0200

    renamed pipline.py to pipeline.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from Model.model import ModelHandler
-from Model.pipline import DataPipeline
+from Model.pipeline import DataPipeline
 
 def load_data():
     """
